e9c3278a4136b34d64c3842f7838193c05f869bc
Gabriel <gabrielstevenw2005@gmail.com>
2025-11-18 13:04:39 +0700

    Update kontak.php

--- kontak.php
+++ kontak.php
@@ -29,6 +29,7 @@
                     </div>
                     <div class="offcanvas-body justify-content-start">
                         <a href="mainpage.php">Beranda</a>
+                        <a href="info_gunung.php">Informasi Gunung</a>
                         <a href="status.php">Cek Status Gunung</a>
                         <a href="sebaran.php">Wilayah Terdampak</a>
                         <a href="dataPosko.php">Posko & Logistik</a>
@@ -303,3 +304,4 @@
 
 
 </html>
+
