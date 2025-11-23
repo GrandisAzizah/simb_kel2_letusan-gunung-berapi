96210e3cce6b706c3684127ce0f9056d3dc130da
GrandisAzizah <124240045@student.upnyk.ac.id>
2025-11-23 21:09:03 +0700

    add nav menu

--- input_sebaran.php
+++ input_sebaran.php
@@ -448,12 +448,26 @@ include 'koneksi.php';
                         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                     </div>
                     <div class="offcanvas-body justify-content-start">
-                        <a href="index.php">Beranda</a>
+                        <a href="mainpage.php">Beranda</a>
                         <a href="status.php">Cek Status Gunung</a>
-                        <a href="sebaran_petugas.php">Wilayah Terdampak</a>
+                        <a href="info_gunung.php">Informasi Status Gunung Berapi</a>
+                        <a href="sebaran.php">Sebaran Wilayah Terdampak</a>
                         <a href="dataPosko.php">Posko & Logistik</a>
-                        <a href="dataKorban.php">Data Korban & Pengungsi</a>
+                        <a href="">Data Korban & Pengungsi</a>
                         <a href="laporan.php">Laporan Kejadian & Riwayat Letusan</a>
+                        <div class="d-grid col-12">
+                            <?php if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true): ?>
+                                <!-- sudah login -->
+                                <a href="input_dataposko.php">Input Data Posko</a>
+                                <a href="input_laporan.php">Input Laporan</a>
+                                <a href="input_sebaran.php">Input Sebaran</a>
+                                <button href="logout.php" class="btn btn-danger mt-1">Logout</button>
+                            <?php else: ?>
+                                <!-- belum login -->
+                                <button href="login.php" class="btn btn-danger mt-3">Login</button>
+                                <button href="registrasi.php" class="btn btn-danger mt-3">Registrasi</button>
+                            <?php endif; ?>
+                        </div>
                     </div>
                 </div>
 
