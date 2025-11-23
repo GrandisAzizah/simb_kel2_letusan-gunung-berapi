de1ed5f46eed0a5c32b485641fa0e7af22a870da
GrandisAzizah <124240045@student.upnyk.ac.id>
2025-11-23 21:07:32 +0700

    add input menu

--- input_dataposko.php
+++ input_dataposko.php
@@ -178,6 +178,9 @@ session_start();
                         <div class="d-grid col-12">
                             <?php if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true): ?>
                                 <!-- sudah login -->
+                                <a href="input_dataposko.php">Input Data Posko</a>
+                                <a href="input_laporan.php">Input Laporan</a>
+                                <a href="input_sebaran.php">Input Sebaran</a>
                                 <button href="logout.php" class="btn btn-danger mt-1">Logout</button>
                             <?php else: ?>
                                 <!-- belum login -->
@@ -854,5 +857,6 @@ session_start();
 </body>
 
 </html>
+
 
 
