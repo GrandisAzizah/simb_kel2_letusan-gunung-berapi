8029d9a1cad32adadcde999eca6b20e4118e598e
GrandisAzizah <124240045@student.upnyk.ac.id>
2025-11-24 19:56:03 +0700

    add link to mainpage

--- template.php
+++ template.php
@@ -52,13 +52,13 @@ session_start();
                 </div>
 
                 <!-- navbar brand and links -->
-                <a class="navbar-brand" href="#">Volcanoes Monitor</a>
+                <a class="navbar-brand" href="mainpage.php">Volcanoes Monitor</a>
             </div>
             <!-- navbar menu -->
             <div class="nav-menu">
                 <ul class="nav justify-content-end">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Beranda</a>
+                        <a class="nav-link active" aria-current="page" href="mainpage.php">Beranda</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="kontak.php">Kontak</a>
@@ -177,3 +177,4 @@ session_start();
 </body>
 
 </html>
+
