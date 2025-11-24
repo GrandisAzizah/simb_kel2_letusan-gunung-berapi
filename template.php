6d6f8bcd94dda9c1826e4e52a2dbf324b4f12034
GrandisAzizah <124240045@student.upnyk.ac.id>
2025-11-24 23:08:45 +0700

    Update template.php

--- template.php
+++ template.php
@@ -31,22 +31,31 @@ session_start();
                         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                     </div>
                     <div class="offcanvas-body justify-content-start">
-                        <a href="mainpage.php">Beranda</a>
-                        <a href="info_gunung.php">Cek Status Gunung</a>
-                        <a href="status.php">Informasi Status Gunung Berapi</a>
-                        <a href="sebaran.php">Sebaran Wilayah Terdampak</a>
-                        <a href="dataPosko.php">Posko & Logistik</a>
-                        <a href="">Data Korban & Pengungsi</a>
-                        <a href="laporan.php">Laporan Kejadian & Riwayat Letusan</a>
-                        <div class="d-grid col-12">
-                            <?php if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true): ?>
-                                <!-- sudah login -->
-                                <a href="logout.php" class="btn btn-danger mt-1 text-white">Logout</a>
-                            <?php else: ?>
-                                <!-- belum login -->
-                                <a href="login.php" class="btn btn-danger mt-3 text-white">Login</a>
-                                <a href="registrasi.php" class="btn btn-danger mt-3 text-white">Registrasi</a>
-                            <?php endif; ?>
+                        <?php if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true): ?>
+                            <!-- sudah login -->
+                            <a href="mainpage.php">Beranda</a>
+                            <a href="info_gunung.php">Cek Status Gunung</a>
+                            <a href="status.php">Informasi Status Gunung Berapi</a>
+                            <a href="sebaran.php">Sebaran Wilayah Terdampak</a>
+                            <a href="dataPosko.php">Posko & Logistik</a>
+                            <a href="">Data Korban & Pengungsi</a>
+                            <a href="laporan.php">Laporan Kejadian & Riwayat Letusan</a>
+                            <a href="input_sebaran.php">Input Sebaran Wilayah Terdampak</a>
+                            <a href="input_dataposko.php">Input Posko & Logistik</a>
+                            <a href="input_laporan.php">Input Laporan Kejadian & Riwayat Letusan</a>
+                            <a href="logout.php" class="btn btn-danger mt-1 text-white">Logout</a>
+                        <?php else: ?>
+                            <!-- belum login -->
+                            <a href="mainpage.php">Beranda</a>
+                            <a href="info_gunung.php">Cek Status Gunung</a>
+                            <a href="status.php">Informasi Status Gunung Berapi</a>
+                            <a href="sebaran.php">Sebaran Wilayah Terdampak</a>
+                            <a href="dataPosko.php">Posko & Logistik</a>
+                            <a href="">Data Korban & Pengungsi</a>
+                            <a href="laporan.php">Laporan Kejadian & Riwayat Letusan</a>
+                            <a href="login.php" class="btn btn-danger mt-3 text-white">Login</a>
+                            <a href="registrasi.php" class="btn btn-danger mt-3 text-white">Registrasi</a>
+                        <?php endif; ?>
                         </div>
                     </div>
                 </div>
@@ -177,3 +186,4 @@ session_start();
 </body>
 
 </html>
+
