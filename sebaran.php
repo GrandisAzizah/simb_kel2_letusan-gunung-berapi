db393a47ec5051166b079c5f05c0553b05697272
Gabriel <gabrielstevenw2005@gmail.com>
2025-11-24 22:24:19 +0700

    Update sebaran.php

--- sebaran.php
+++ sebaran.php
@@ -502,7 +502,7 @@ if (!$result) {
                         <a href="info_gunung.php">Informasi Status Gunung Berapi</a>
                         <a href="sebaran.php">Sebaran Wilayah Terdampak</a>
                         <a href="dataPosko.php">Posko & Logistik</a>
-                        <a href="">Data Korban & Pengungsi</a>
+                        <a href="dataKorban.php">Data Korban & Pengungsi</a>
                         <a href="laporan.php">Laporan Kejadian & Riwayat Letusan</a>
                         <div class="d-grid col-12">
                             <?php if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true): ?>
